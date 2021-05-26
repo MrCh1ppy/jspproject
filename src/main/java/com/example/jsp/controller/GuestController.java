d806079566e9d5083ff201246ae1b760a4ef51db
Wanyan <www51875187@163.com>
2021-05-26 16:06:30 +0800

    Guest、Store: controller、service

---
+++ src/main/java/com/example/jsp/controller/GuestController.java
@@ -0,0 +1,41 @@
+package com.example.jsp.controller;
+
+import java.util.List;
+import com.example.jsp.commons.exception.ProjectException;
+import com.example.jsp.commons.model.Transporter;
+import com.example.jsp.pojo.Address;
+import com.example.jsp.pojo.Guest;
+import com.example.jsp.pojo.User;
+import com.example.jsp.service.GuestService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GuestController {
+    private GuestService guestService;
+
+    @Autowired
+    public void setGuestService (GuestService guestService) {
+        this.guestService = guestService;
+    }
+
+    @GetMapping("/enroll/{username}/{password}/{name}/{address}/{telephone}")
+    public Transporter enroll(@PathVariable("username")String username,
+                              @PathVariable("password")String password,
+                              @PathVariable("name")String name,
+                              @PathVariable("address")List<Address> addresses,
+                              @PathVariable("telephone")String telephone)throws ProjectException {
+        final var user = new User()
+                .setUsername(username)
+                .setPassword(password);
+        final var guest = new Guest()
+                .setName(name)
+                .setTelephone(telephone)
+                .setAddresses(addresses)
+                .setLoginUser(user);
+        guestService.enroll(guest,user);
+        return new Transporter().setMsg("注册成功");
+    }
+}
--- src/main/java/com/example/jsp/service/GuestService.java
+++ src/main/java/com/example/jsp/service/GuestService.java
@@ -2,10 +2,12 @@ package com.example.jsp.service;
 
 import com.example.jsp.commons.exception.ProjectException;
 import com.example.jsp.pojo.Guest;
+import com.example.jsp.pojo.User;
 
 /**
  * @author 橙鼠鼠
  */
 public interface GuestService {
 	void create(Guest target)throws ProjectException;
+	void enroll(Guest guest, User user)throws ProjectException;
 }

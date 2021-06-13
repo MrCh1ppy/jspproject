5524decbb511d7406d452551c2e189fca7abe8c6
橙鼠鼠 <62237330+c737713@users.noreply.github.com>
2021-06-13 12:49:23 +0800

    添加了关于权限与登录错误的全局异常拦截器

--- src/main/java/com/example/jsp/utils/execption/ErrorInfo.java
+++ src/main/java/com/example/jsp/utils/execption/ErrorInfo.java
@@ -21,6 +21,11 @@ public class ErrorInfo {
 		INFO.put("303", "创建Store时,对应的User没有创建成功");
 		INFO.put("304", "创建Store时,对应的User没有创建成功");
 		INFO.put("305", "创建Product时,没有对应的Store");
+		//登录错误:5
+		//鉴权错误:6
+	}
+
+	private ErrorInfo () {
 	}
 
 	public static Map<String, String> getInfo () {

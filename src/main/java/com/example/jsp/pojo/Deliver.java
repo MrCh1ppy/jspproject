fef72be720095452811736fd1a14bcd5b4a60d37
death <2564908936@qq.com>
2021-06-15 01:02:45 +0800

    bug dealing

--- src/main/java/com/example/jsp/pojo/Deliver.java
+++ src/main/java/com/example/jsp/pojo/Deliver.java
@@ -2,6 +2,7 @@ package com.example.jsp.pojo;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.*;
+import lombok.experimental.Accessors;
 
 import java.io.Serializable;
 
@@ -9,8 +10,10 @@ import java.io.Serializable;
  * @author 橙鼠鼠
  */
 @Getter
+@Setter
 @JsonIgnoreProperties(value = {"handler"})
 @EqualsAndHashCode(callSuper = false)
+@Accessors(chain = true)
 @ToString
 @AllArgsConstructor
 @NoArgsConstructor
@@ -19,20 +22,4 @@ public class Deliver implements Serializable {
 	private String name;
 	private String telephone;
 	private User loginUser;
-
-
-	public Deliver setName (String name) {
-		this.name = name;
-		return this;
-	}
-
-	public Deliver setTelephone (String telephone) {
-		this.telephone = telephone;
-		return this;
-	}
-
-	public Deliver setLoginUser (User loginUser) {
-		this.loginUser = loginUser;
-		return this;
-	}
 }

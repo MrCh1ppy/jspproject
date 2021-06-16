d49df51931ccfd79a898bfb02e1c59c01af77253
橙鼠鼠 <62237330+c737713@users.noreply.github.com>
2021-06-16 11:21:48 +0800

    order.show()
    增加了order/show 方法

--- src/main/java/com/example/jsp/service/OrderService.java
+++ src/main/java/com/example/jsp/service/OrderService.java
@@ -5,6 +5,8 @@ import com.example.jsp.pojo.Order;
 import com.example.jsp.pojo.OrderInfo;
 import com.example.jsp.pojo.Product;
 
+import java.util.List;
+
 
 public interface OrderService {
 	void create (Order order) throws ProjectException;
@@ -16,6 +18,8 @@ public interface OrderService {
 	void restore (Order order) throws ProjectException;
 
 	Order select (Integer orderId) throws ProjectException;
+
+	List<Order> select();
 
 	OrderService addProduct (Order order, Integer productId, Integer num) throws ProjectException;
 
